b92dc03525c1f39b54e36520e9d0988b177cb51d
Răzvan Nicolae Căpățînă <razvannicolae48@gmail.com>
2024-11-17 18:38:53 +0200

    added tasks files

---
+++ Tema-1/Task_3.py
@@ -0,0 +1,10 @@
+import numpy as np
+import cv2 as cv
+
+
+class Task_3:
+
+    def __init__(self):
+        pass
+
+
---
+++ Tema-1/generatorSabloane.py
@@ -0,0 +1,15 @@
+import numpy as np
+import cv2 as cv
+
+class GeneratorSabloane:
+
+
+    def __init__(self, adresaDirectorImagini : str, extensieImagini : str, adresaDirectorSalvareSabloane : str):
+        self.adresaDirectorImagini = adresaDirectorImagini
+        self.extensieImagini = extensieImagini
+        self.adresaDirectorSalvareSabloane = adresaDirectorSalvareSabloane
+
+
+    def ruleaza(self):
+        pass
+
