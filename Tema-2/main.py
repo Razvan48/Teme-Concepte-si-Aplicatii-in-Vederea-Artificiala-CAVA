314ea422b94ebb90f954f45b22c21ee8819814b7
Răzvan Nicolae Căpățînă <razvannicolae48@gmail.com>
2024-12-20 21:31:32 +0200

    added all datasets and tema-2

---
+++ Tema-2/main.py
@@ -0,0 +1,8 @@
+
+
+print('test')
+
+
+# TODO:
+# de facut documentatia (txt-ul) (momentan in documentatie trebuie scris doar ce module sunt necesare (nici asta nu e scris momentan))
+# de facut latex-ul
